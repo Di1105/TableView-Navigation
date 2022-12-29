458dc08f2cdc228008722a7152c6150eb486bf28
Di1105 <delcioglu@gmail.com.com>
2022-12-29 11:27:06 +0300

    Initial Commit

---
+++ Deneme2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Deneme2
+//
+//  Created by Dilara Elçioğlu on 29.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
